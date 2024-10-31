5eb78749f7617f43fa9aa071175a34526851c061
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-10-31 13:36:05 +0100

    edited board distrubition + add card component

--- src/app/board/distribution/distribution.component.ts
+++ src/app/board/distribution/distribution.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-distribution',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './distribution.component.html',
-  styleUrl: './distribution.component.scss'
+  styleUrls: ['./distribution.component.scss']
 })
 export class DistributionComponent {
+  taskStatuses = [
+    { label: 'To do', isHovered: false, isAdding: false, tasks: ['1'] },
+    { label: 'In progress', isHovered: false, isAdding: false, tasks: [] },
+    { label: 'Await feedback', isHovered: false, isAdding: false, tasks: [] },
+    { label: 'Done', tasks: [] },
+  ];
 
+
+  onMouseOver(status: any) {
+    status.isHovered = true;
+  }
+
+
+  onMouseLeave(status: any) {
+    status.isHovered = false;
+  }
+
+
+  toggleAddTask(status: any) {
+    status.isAdding = !status.isAdding;
+  }
 }

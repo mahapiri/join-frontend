4c741eca199f34dc1b08f78f389e83d15605f217
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-10-31 15:20:42 +0100

    edited card

--- src/app/board/card/card.component.ts
+++ src/app/board/card/card.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
 

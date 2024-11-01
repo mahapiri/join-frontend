457ced3d45c0d176bb2e7be700f14b9cded5d41a
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-11-01 14:03:33 +0100

    create all popup comp + service

---
+++ src/app/popups/adding/adding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adding',
+  standalone: true,
+  imports: [],
+  templateUrl: './adding.component.html',
+  styleUrl: './adding.component.scss'
+})
+export class AddingComponent {
+
+}

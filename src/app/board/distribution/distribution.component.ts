9470f422928821d5edc97e7efaf767f6093625f8
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-03-10 15:32:52 +0100

    edited dashboard + edited vw/vh to %

--- src/app/board/distribution/distribution.component.ts
+++ src/app/board/distribution/distribution.component.ts
@@ -2,27 +2,45 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { TaskService } from '../../services/task.service';
+import { CdkDragDrop, CdkDrag, CdkDropList, moveItemInArray, transferArrayItem, DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-distribution',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, DragDropModule, CdkDrag],
   templateUrl: './distribution.component.html',
   styleUrls: ['./distribution.component.scss']
 })
 export class DistributionComponent {
   taskStatuses = [
-    { label: 'To do', isHovered: false, isAdding: false, tasks: ['1'] },
-    { label: 'In progress', isHovered: false, isAdding: false, tasks: [] },
-    { label: 'Await feedback', isHovered: false, isAdding: false, tasks: [] },
-    { label: 'Done', tasks: [] },
+    { id: 'to-do', label: 'To do', isHovered: false, isAdding: false, tasks: ['Task 1', 'Task 2'] },
+    { id: 'in-progress', label: 'In progress', isHovered: false, isAdding: false, tasks: ['Task 3'] },
+    { id: 'await-feedback', label: 'Await feedback', isHovered: false, isAdding: false, tasks: [] },
+    { id: 'done', label: 'Done', isHovered: false, isAdding: false, tasks: [] },
   ];
+  connectedToIds = this.taskStatuses.map(s => s.id);
+  isDragging: boolean = false;
 
   constructor(private task: TaskService) {
 
   }
 
-  isDragging: boolean = false;
+
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log('Drop Event:', event);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 
 
   onMouseOver(status: any) {
@@ -37,9 +55,5 @@ export class DistributionComponent {
 
   toggleAddTask(status: any) {
     status.isAdding = !status.isAdding;
-  }
-
-  startDragging() {
-    this.isDragging = true;
   }
 }
